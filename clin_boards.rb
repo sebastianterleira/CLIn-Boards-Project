99650a3bd32b8f2b3a5c1562cbc01b4f5987ed61
Andres Naula <andresnaula@gmail.com>
2022-08-26 10:35:08 -0500

    ADD: add Show method and refactor some methods to funciton accordingly

--- clin_boards.rb
+++ clin_boards.rb
@@ -12,12 +12,12 @@ class ClinBoards
     welcome_message
     action = ""
     until action == "exit"
-      print_table(list:@boards, title: "CLIn Boards", headings: ["ID", "Name", "Description", "List(#cards)"])
-      
-      action, id = menu(["create", "show ID", "update ID", "delete ID", "exit" ]) 
+      print_table(list: @boards, title: "CLIn Boards", headings: ["ID", "Name", "Description", "List(#cards)"])
+
+      action, id = menu(["Board options: "], [["create", "show ID", "update ID", "delete ID", "exit"]])
 
       case action
-      when "create" then create_board(@boards)
+      when "create" then puts "Create Board" #create_playlist
       when "show" then puts "Show Board #{id}"  #update_playlist(id)
       when "update" then puts "Update Board #{id}"  #show_playlist(id)
       when "delete" then puts "Delete Board #{id}"  #delete_playlist(id)
@@ -26,24 +26,7 @@ class ClinBoards
         puts "Invalid action"
       end
     end
-    # puts action
-  end
-
-  private
-
-  def create_board(boards)
-    board_hash = board_form
-    new_board = Boards.new(**board_hash)
-    boards.push(new_board)
-    File.write(@filename, boards.to_json)
-  end
-
-  def board_form
-    print "Name: "
-    name = gets.chomp
-    print "Description: "
-    description = gets.chomp
-    { name: name, description: description }
+    puts action
   end
 
   def welcome_message
@@ -52,11 +35,15 @@ class ClinBoards
     puts "#" * 36
   end
 
-  def menu(options)
-    puts "Board options: #{options.join(" | ")}"
+  def menu(message, options)
+    message.each_with_index do |_m, i|
+      puts "#{message[i]}#{options[i].join(' | ')}"
+    end
     print "> "
-    action, id = gets.chomp.split #=> "show 1" ~> ["show", "1"]
-    [action, id.to_i] # return implicito ~> ["show", 1]
+    action, arg = gets.chomp.split
+    arg = "" if arg.nil?
+    arg = arg.to_i if arg.match(/\d/)
+    [action, arg]
   end
 
   def print_table(list:, title:, headings:)
@@ -72,9 +59,35 @@ class ClinBoards
     data.map { |board_hash| Boards.new(**board_hash) }
   end
 
+  def show_board(id)
+    board = @boards.find { |b| b.id == id }
+    action = ""
+    until action == "back"
+
+      board.lists.each do |list|
+        print_table(title: list.name, headings: ["ID", "Title", "Members", "Labels", "Due Date", "Checklist"],
+                    list: list.cards)
+      end
+
+      action, arg = menu(["List options: ", "Card options: ", ""],
+                         [["create-list", "update-list LISTNAME", "delete-list LISTNAME"],
+                          ["create-card", "checklist ID", "update-card ID", "delete-card ID"], ["back"]])
+
+      case action
+      when "create-list" then puts "create-list!"
+      when "update-list" then puts "update-list! #{arg}"
+      when "delete-list" then puts "Udelete-list! #{arg}"
+      when "create-card" then puts "create-card!"
+      when "checklist" then puts "create-card! #{arg}"
+      when "update-card" then puts "udate-card! #{arg}"
+      when "delete-card" then puts "delete-card! #{arg}"
+      else
+        puts "Invalid action" unless action == "back"
+      end
+    end
+  end
 end
 
-# get the command-line arguments if neccesary
 filename = ARGV.shift
 ARGV.clear
 
