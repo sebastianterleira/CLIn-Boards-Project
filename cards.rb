3347b71d7238b78b27749a7f583fc23972df6808
Martha Huaman <ms.huamann@gmail.com>
2022-08-27 17:17:49 -0500

    ADD update to_json method

--- cards.rb
+++ cards.rb
@@ -2,7 +2,7 @@ class Cards
   attr_reader :id, :title, :members, :labels, :due_date, :checklist
   @@id_count = 0
   def initialize(id:nil, title:, members:[], labels:[], due_date:, checklist:[])
-    @id = id || (@@id_count + 1)
+    @id = id ? id : @@id_count + 1
     @@id_count = @id
     @title = title
     @members = members
@@ -10,11 +10,16 @@ class Cards
     @due_date = due_date
     @checklist = checklist
   end
-
   def details
     count = 0
     @checklist.map { |chk| count += 1 if chk[:completed] }
     [@id, @title, @members.join(", "), @labels.join(", "), @due_date, "#{count}/#{@checklist.size}"]
+  end
+  def update( title:, members:[], labels:[], due_date: )
+    @title = title
+    @members = members
+    @labels = labels
+    @due_date = due_date
   end
   def to_json(_arg)
     { title: @title, members:@members, id: @id, labels: @labels, due_date: @due_date, checklist: @checklist}.to_json
