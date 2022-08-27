0842274c8ddda8b9d76d288a9614425646310410
Martha Huaman <ms.huamann@gmail.com>
2022-08-27 17:17:35 -0500

    ADD update to_json method

--- lists.rb
+++ lists.rb
@@ -1,19 +1,20 @@
 require_relative "cards"
-
+require_relative "cards"
 class Lists
   attr_reader :id, :name, :cards
   @@id_count = 0
   def initialize(id:nil, name:, cards:[])
-    @id = id || (@@id_count + 1)
+    @id = id ? id : @@id_count + 1
     @@id_count = @id
     @name = name
     @cards = load_cards(cards)
   end
-
   def load_cards(cards)
     cards.map { |card_hash| Cards.new(**card_hash) }
   end
-
+  def update(name:)
+    @name=name unless name.empty?
+  end
   def to_json(_arg)
     { name: @name, id: @id, cards: @cards}.to_json
   end
